9af9b74bcfb88606922573ab687586867fe459da
khushikaa18 <154812853+khushikaa18@users.noreply.github.com>
2025-08-31 17:32:04 +0530

    Time: 5 ms (64.99%) | Memory: 58.3 MB (47.69%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,13 +1,14 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        Arrays.sort(nums);
+        /*Arrays.sort(nums);
         for(int i=1;i<nums.length;i++){
            if(nums[i]==nums[i-1]){
             return nums[i];
            }
         }
-        return 0;
-        /*int slow=nums[0];
+        return 0;*/
+        //Floyd warshall cycle hare and tortoise method
+        int slow=nums[0];
         int fast=nums[0];
         do{
             slow=nums[slow];
@@ -20,6 +21,6 @@ class Solution {
             slow=nums[slow];
             fast=nums[fast];
         }
-        return fast;*/
+        return fast;
     }
 }
