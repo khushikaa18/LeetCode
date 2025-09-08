7583dd259de2cc93bed00fc2140a9fd513fdc43f
khushikaa18 <154812853+khushikaa18@users.noreply.github.com>
2025-09-08 18:49:24 +0530

    Time: 1 ms (81.84%) | Memory: 45.8 MB (20.49%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int index=0,index1=0;;
+        int index=0,index1=0;
         for(int i=0;i<nums.length;i++){
             index=(index^nums[i])&~index1;
             index1=(index1^nums[i])&~index;
