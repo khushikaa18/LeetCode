dc185ae8df23eb84f6ff492a80daae9396a036f6
khushikaa18 <154812853+khushikaa18@users.noreply.github.com>
2026-01-15 19:36:20 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (8.86%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,33 @@
+class Solution {
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try (FileWriter fw = new FileWriter("display_runtime.txt")) {
+                fw.write("000");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }));
+    }
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } 
+            else {
+                if (stack.isEmpty()) return false;
+
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
