5fe4ff082a89899adb00d104717b2deb7923b904
khushikaa18 <154812853+khushikaa18@users.noreply.github.com>
2026-03-11 14:42:57 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (85.10%) - LeetSync

---
+++ 476-number-complement/number-complement.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        a=format(num, 'b')
+        inverse_s = ''.join(['1' if i == '0' else '0' for i in a])
+        b=int(inverse_s,2)
+
+        return b 
