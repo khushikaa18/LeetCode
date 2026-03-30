cd1d904774358226719482e83609b5a9d2c827fe
khushikaa18 <154812853+khushikaa18@users.noreply.github.com>
2026-03-30 20:31:35 +0530

    Time: 3 ms (72.16%) | Memory: 19.4 MB (69.39%) - LeetSync

---
+++ 139-word-break/word-break.py
@@ -0,0 +1,21 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordSet=set(wordDict) # Hashing 
+        memo={} # store already computed results
+
+        def dfs(i):
+            if i==len(s):
+                return True
+            
+            if i in memo:
+                return memo[i]
+            
+            for j in range(i + 1, len(s) + 1):
+                if s[i:j] in wordSet and dfs(j):
+                    memo[i] = True
+                    return True
+            
+            memo[i] = False
+            return False
+        
+        return dfs(0)
