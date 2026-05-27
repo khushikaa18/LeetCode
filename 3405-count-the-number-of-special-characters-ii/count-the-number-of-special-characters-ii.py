a9a4b51e569ae53f8207c2e41b7a32ffc719688d
Khushika Surana <154812853+khushikaa18@users.noreply.github.com>
2026-05-27 14:12:48 +0530

    Time: 251 ms (61.77%) | Memory: 21.1 MB (98.82%) - LeetSync

---
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        lower={}
+        upper={}
+
+        for i,ch in enumerate(word):
+            if ch.islower():
+                lower[ch]=i
+            
+            elif ch.lower() not in upper:
+                upper[ch.lower()]=i
+            
+        count=0
+
+        for ch in lower:
+
+            if ch in upper and lower[ch]<upper[ch]:
+                count+=1
+        
+        return count
