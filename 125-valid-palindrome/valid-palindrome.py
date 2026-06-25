db229edc4ad9debad3f29a9eb507c9e71de9c9f2
Khushika Surana <154812853+khushikaa18@users.noreply.github.com>
2026-06-25 21:04:10 +0530

    Time: 7 ms (81.01%) | Memory: 19.6 MB (77.12%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,16 +4,17 @@ class Solution:
         right=len(s)-1
 
         while left<=right:
-            while left < right and not s[left].isalnum():
+            if not s[left].isalnum():
                 left+=1
 
-            while left < right and not s[right].isalnum():
+            elif not s[right].isalnum():
                 right-=1
 
-            if s[left].lower() != s[right].lower():
+            elif s[left].lower() != s[right].lower():
                 return False
             
-            left+=1
-            right-=1
+            else:
+                left+=1
+                right-=1
         
         return True
